139f622ae7c5c45f4a3890af1bd2289d94131f09
susondra <123081753+susondra@users.noreply.github.com>
2025-03-19 01:40:06 +0800

    nearly basic function - safe

--- parse.py
+++ parse.py
@@ -10,9 +10,9 @@ from xml.dom import minidom
 grammar = '''
 program: class*                                        # pravidlo 1,2
 
-class: "class" CID ":" CID "{" method "}"              # pravidlo 3,5; method* resi, ze muze byt 0 nebo vice method
+class: "class" CID ":" CID "{" method* "}"              # pravidlo 3,5; method* resi, ze muze byt 0 nebo vice method
 
-method: selector block method |                                 # pravidlo 4; method? - volitelne, muze a nemusi byt dalsi metoda
+method: selector block                                # pravidlo 4; method? - volitelne, muze a nemusi byt dalsi metoda
 
 selector: ID | IDCOLON selector_tail*                         # pravidlo 7,9
 selector_tail: IDCOLON                                   # pravidlo 8
@@ -26,7 +26,6 @@ expr_tail: ID | expr_sel                                       # pravidlo 16
 expr_sel: IDCOLON expr_base expr_sel |                          # pravidlo 18
 
 expr_base: INT | STRING | ID | CID | block | "(" expr ")" | KEYWORD         # pravidlo 20,21,22
-
 
 IDCOLON: ID ":"
 COLONID: ":" ID
@@ -57,14 +56,16 @@ class ASTToXML:
             self.root.set('description', description)
 
     def convert(self, node):
-        # Skip the 'program' node and start processing from its children
         for child in node.children:
             self._convert_node(child, self.root)
 
-    def _convert_node(self, node, parent_elem, selector_name=None, par_number=None):
+    def _convert_node(self, node, parent_elem, selector_name=None, par_count=0):
         if isinstance(node, Token):
             if node.type == 'ID':
                 elem = ET.SubElement(parent_elem, "var", name=node.value)
+            elif node.type == 'INT':
+                attributes = {"class": "Integer", "value": node.value}
+                elem = ET.SubElement(parent_elem, "literal", **attributes)
             else:
                 elem = ET.SubElement(parent_elem, "token", type=node.type)
                 elem.text = node.value
@@ -77,24 +78,29 @@ class ASTToXML:
                     self._convert_node(child, elem)
             elif node.data == "method":
                 selector = node.children[0].children[0].value
+                try:
+                    for child in node.children[0].children[1:]:
+                        selector += child.children[0].value
+                except:
+                    pass
+                arity = selector.count(":")
                 elem = ET.SubElement(parent_elem, "method", selector=selector)     
-                self._convert_node(node.children[1], elem, selector)
-            elif node.data == "selector":
-                # Skip processing the selector node itself
-                for child in node.children:
-                    self._convert_node(child, parent_elem)
+                self._convert_node(node.children[1], elem, selector, arity)
             elif node.data == "block":
-                arity = selector_name.count(":") if selector_name else 0
-                elem = ET.SubElement(parent_elem, "block", arity=str(arity))
+                elem = ET.SubElement(parent_elem, "block", arity=str(par_count))
                 if node.children[0].children:
-                    self._convert_node(node.children[0], elem)
+                    self._convert_node(node.children[0], elem, par_count=par_count)
+                elif par_count != 0:
+                    sys.exit(22)
                 if node.children[1].children:
                     self._convert_node(node.children[1], elem)
             elif node.data == "block_par":
-                par_counter = 1
+                par_counter = 0
                 for child in node.children:
-                    elem = ET.SubElement(parent_elem, "parameter", order=str(par_counter), name=child[1:])
                     par_counter += 1
+                    elem = ET.SubElement(parent_elem, "parameter", order=str(par_counter), name=child[1:])
+                if par_counter != par_count:
+                    sys.exit(22)
             elif node.data == "block_stat":
                 assign_counter = 1
                 for child in node.children[::2]:
@@ -103,15 +109,30 @@ class ASTToXML:
                     self._convert_node(node.children[2*assign_counter-1], elem)
                     assign_counter += 1
             elif node.data == "expr":
-                if node.children[1]:
-                    selector = self._build_selector(node.children[1])
-                else:
-                    selector = "mozna chyba"
-                elem = ET.SubElement(parent_elem, "send", selector=selector)
+                expr_elem = ET.SubElement(parent_elem, "expr")
+                try:                    
+                    selector = node.children[1].children[0].value
+                    elem = ET.SubElement(expr_elem, "send", selector=selector)
+                    self._convert_node(node.children[0], elem)
+                except:
+                    if node.children[1]:    # pokud je expr->expr_tail->expr_sel
+                        selector = self._build_selector(node.children[1])
+                        par_count = selector.count(":") if selector else 0
+                    else:
+                        sys.exit(22) # chyba syntaxe
+                    elem = ET.SubElement(expr_elem, "send", selector=selector)
+                    self._convert_node(node.children[0], elem)
+                    try:
+                        par_count_check = self._convert_send_arguments(node.children[1].children[0], elem)
+                        if par_count != par_count_check:
+                            print(par_count,"!=", par_count_check)
+                            sys.exit(22)
+                    except:
+                        sys.exit(22)
+            elif node.data == "expr_base":
+                #print(node.children)
+                elem = ET.SubElement(parent_elem, "expr")
                 self._convert_node(node.children[0], elem)
-                for i, child in enumerate(node.children[1:], start=1):
-                    arg_elem = ET.SubElement(elem, "arg", order=str(i))
-                    self._convert_node(child, arg_elem)
             else: 
                 elem = ET.SubElement(parent_elem, node.data)
                 for child in node.children:
@@ -123,22 +144,29 @@ class ASTToXML:
         elem.text = str(node)
         return elem
 
+    def _convert_send_arguments(self, node, parent_elem, order=0):
+        if node.children:
+            order += 1
+            arg_elem = ET.SubElement(parent_elem, "arg", order=str(order))
+            if isinstance(node.children[1].children[0], Token):
+                self._convert_node(node.children[1], arg_elem)                
+            else:
+                self._convert_node(node.children[1].children[0], arg_elem)
+            return self._convert_send_arguments(node.children[2], parent_elem, order)
+        return order
+
     def _build_selector(self, node):
-        print("build_selector ", node)
         if isinstance(node, Token):
-            print("Token value")
             return node.value
-        if node.data == "selector_tail":
-            print("tail")
+        if isinstance(node, Tree) and node.data == "expr_tail":
             try:
-                selector = self._build_selector(node.children[0])   
+                selector = self._build_selector(node.children[0])
                 return selector
             except:
                 return "chyba, nenalezeno"
         if isinstance(node, Tree) and node.data == "expr_sel":
-            print("sel")
             selector = node.children[0].value
-            if node.children:
+            if node.children[2].children:
                 selector += self._build_selector(node.children[2])
             return selector
         return "chyba, failed _build_selector" # sys.exit(22) asi, jestli je toto chyba syntaxe
@@ -149,10 +177,25 @@ class ASTToXML:
         pretty_xml = reparsed.toprettyxml(indent="  ")              # finalne ho pomoci fnc zkraslim
         return '<?xml version="1.0" encoding="UTF-8"?>\n' + pretty_xml.split('\n', 1)[1]
 
+##--------------------------------------
+def find_selector_tail(node):
+    if isinstance(node, Tree) and node.data == "selector_tail":
+        return node  # Found the selector_tail
+    if hasattr(node, "children"):  # Check if the node has children
+        for child in node.children:
+            result = find_selector_tail(child)  # Recursively search in children
+            if result:
+                return result
+    return None  # Not found
+##--------------------------------------
+
 def parse_file(source):
-    parser = Lark(grammar, start='program', parser='lalr')
-    # bude se ridit gramatikou popsanou v grammer, zacne se v pravidlem program, parser='lalr' - vyuzije se jako typ parseru LALR(1), ktery je efektivnejsi
-    return parser.parse(source)
+    try:
+        parser = Lark(grammar, start='program', parser='lalr')
+        # bude se ridit gramatikou popsanou v grammer, zacne se v pravidlem program, parser='lalr' - vyuzije se jako typ parseru LALR(1), ktery je efektivnejsi
+        return parser.parse(source)
+    except:
+        sys.exit(21) # chyba lexikalni nebo syntakticka?
 
 if __name__ == "__main__":
     if len(sys.argv) > 2:
